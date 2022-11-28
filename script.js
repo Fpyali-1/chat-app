3479c02a0f5824faed26f75c7b5bac31eaa6d932
Fpyali-1 <77771899+Fpyali-1@users.noreply.github.com>
2022-11-28 14:48:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const ws = new WebSocket("wss://ChillyPlaintiveFact.yaliwainstain.repl.co");
+
+const name1 = prompt("enter your name");
+
+let x = 0;
+
+ws.addEventListener("open", () => {
+  x = 1;
+});
+ws.addEventListener("message", (e) => {
+  const boxes = document.querySelectorAll(".message");
+
+  boxes.forEach((box) => {
+    box.remove();
+  });
+  console.log(e.data);
+  var x = eval(e.data);
+  document.getElementById("messages").innerHTML = "";
+  x.forEach((element) => {
+    console.log(element);
+    var y = `
+  <div class="message">
+  <p>
+  ${element}
+  </p>
+  </div>
+  `;
+    document.body.insertAdjacentHTML("beforeend", y);
+  });
+});
+function send() {
+  ws.send(`"${name1}: ${document.getElementById("input1").value}"`);
+  document.getElementById("input1").value = "";
+  //ws.send()
+}
+addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    send();
+  }
+});
